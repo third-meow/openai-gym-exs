eb9ae6d7da2056c129ecb6a340b20f393a22443c
third-meow <jsheaandcats@gmail.com>
2018-10-11 15:49:08 +1300

    tweaked normalization

--- cartpole/raw_to_normal.py
+++ cartpole/raw_to_normal.py
@@ -7,12 +7,26 @@ ytrain = pickle.load(open('saved/raw/ytrain.pickle', 'rb'))
 xtest = pickle.load(open('saved/raw/xtest.pickle', 'rb'))
 ytest = pickle.load(open('saved/raw/ytest.pickle', 'rb'))
 
-for train_set in xtrain: 
-    train_set = normalize(train_set)
-for test_set in xtest:
-    test_set = normalize(test_set)
+new_train = []
+new_test = []
 
-pickle.dump(xtrain, open('saved/normal/xtrain.pickle', 'wb'))
+def unpack(inp_arr):
+    return inp_arr[0]
+
+for train_set in xtrain: 
+    new_set = train_set.flatten()
+    new_set = normalize(new_set)
+    new_train.append(unpack(new_set))
+
+for test_set in xtest:
+    new_set = test_set.flatten()
+    test_set = normalize(new_set)
+    new_test.append(new_set)
+
+new_train = np.array(new_train)
+new_test = np.array(new_test)
+
+pickle.dump(new_train, open('saved/normal/xtrain.pickle', 'wb'))
 pickle.dump(ytrain, open('saved/normal/ytrain.pickle', 'wb'))
-pickle.dump(xtest, open('saved/normal/xtest.pickle', 'wb'))
+pickle.dump(new_test, open('saved/normal/xtest.pickle', 'wb'))
 pickle.dump(ytest, open('saved/normal/ytest.pickle', 'wb'))
