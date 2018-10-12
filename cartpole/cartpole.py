3068c402af429b5a075eb37d8e8471d97e25dc9c
The third meow <jsheaandcats@gmail.com>
2018-10-12 20:18:06 +1300

    rejigged to work with new model

--- cartpole/cartpole.py
+++ cartpole/cartpole.py
@@ -12,7 +12,7 @@ mdl = keras.models.load_model('saved/latest_model.h5')
 env = gym.make('CartPole-v0')
 env.reset()
 
-fail_n = 0 
+failed = False 
 
 def push_to_prev(prev, new):
     prev[2] = prev[1]
@@ -20,33 +20,48 @@ def push_to_prev(prev, new):
     prev[0] = new
     return prev
 
-def unpack(packed):
-    return np.argmax(packed)
 
-def predict(in_data):
-    keras.utils.normalize(in_data)
+#obs, total_reward, done, ifo = env.step(env.action_space.sample())
 
-
-obs, total_reward, done, ifo = env.step(env.action_space.sample())
-#env.render()
-
-for _ in range(100):
+#over 100 games
+for game_n in range(100):
+    total_reward = 0
     obs = env.reset()
-    prev_obs = np.empty([3])
-    prev_obs = np.append(prev_obs, [obs])
-    prev_obs = np.append(prev_obs, [obs])
-    prev_obs = np.append(prev_obs, [obs])
+    prev_obs = np.array([obs, obs, obs])
     while True:
-        print(prev_obs)
-        print(prev_obs.shape)
+        #prepare prev_obs for model
+        model_input = []
+        model_input.append(prev_obs.flatten())
+        model_input = keras.utils.normalize(model_input)
+        model_input = np.array(model_input)
+
         #predict step
-        nstep = mdl.predict(prev_obs)
-        print(nstep)
+        nstep = int(round(mdl.predict(model_input)[0][0], 0))
+
         #take step
-        #record obs
+        obs, reward, done, _ = env.step(nstep)
+
+        #add reward to total
+        total_reward += reward
+
         #update prev obs
+        prev_obs = push_to_prev(prev_obs, obs)
+
         #if done break
-        break
+        if done:
+            if total_reward < 195:
+                #print fail message
+                print('Game {} failed with a total reward of {}'.format(game_n, 
+                    total_reward))
+                #set fail flag
+                failed = True
+            break
+
+#if model sucdeeded
+if failed == False:
+    print("Success!")
+    print("Over 100 games, no game failed at less than 195 reward")
+
 
 
 #save model
